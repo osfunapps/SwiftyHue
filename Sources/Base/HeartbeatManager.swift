bae7a3347ff71cdba83e40c059022a94572d8c77
Oz <admin@os-apps.com>
2021-05-31 22:32:48 +0300

    Update HeartbeatManager.swift

--- Sources/Base/HeartbeatManager.swift
+++ Sources/Base/HeartbeatManager.swift
@@ -91,11 +91,10 @@ public class HeartbeatManager {
 
         print("Heartbeat Request", "\(url)")
         
-        Alamofire.request(url).responseJSON { response in // method
+        AF.request(url).responseJSON { response in // method
             
             switch response.result {
             case .success:
-                
                 self.handleSuccessResponseResult(response.result, resourceType: resourceType)
                 self.notifyAboutLocalConnection()
                 
@@ -110,21 +109,22 @@ public class HeartbeatManager {
     
     // MARK: Timer Action Response Handling
     
-    private func handleSuccessResponseResult(_ result: Result<Any>, resourceType: HeartbeatBridgeResourceType) {
+    private func handleSuccessResponseResult(_ result: Result<Any, AFError>, resourceType: HeartbeatBridgeResourceType) {
         
         print("Heartbeat Response for Resource Type \(resourceType.rawValue.lowercased()) received")
         //Log.trace("Heartbeat Response: \(resourceType.rawValue.lowercaseString): ", result.value)
         
-        if responseResultIsPhilipsAPIErrorType(result: result, resourceType: resourceType) {
-            
-            if let resultValueJSONArray = result.value as? [JSON] {
+        if case .success(let data) = result {
+            if responseResultIsPhilipsAPIErrorType(result: result, resourceType: resourceType) {
                 
-                self.handleErrors(resultValueJSONArray)
-            }
-            
+                
+                if let resultValueJSONArray = data as? [JSON] {
+                    
+                    self.handleErrors(resultValueJSONArray)
+                }
         } else {
             
-            if let resultValueJSON = result.value as? JSON {
+            if let resultValueJSON = data as? JSON {
                 
                 for heartbeatProcessor in self.heartbeatProcessors {
                     heartbeatProcessor.processJSON(resultValueJSON, forResourceType: resourceType)
@@ -134,23 +134,24 @@ public class HeartbeatManager {
                 
             }
         }
-        
     }
+}
     
-    private func responseResultIsPhilipsAPIErrorType(result: Result<Any>, resourceType: HeartbeatBridgeResourceType) -> Bool {
-        
-        switch resourceType {
+    private func responseResultIsPhilipsAPIErrorType(result: Result<Any, AFError>, resourceType: HeartbeatBridgeResourceType) -> Bool {
+        if case .success(let data) = result {
+            switch resourceType {
             
-        case .config:
-            
-            if let resultValueJSON = result.value as? JSON {
+            case .config:
                 
-                return resultValueJSON.count <= 8 // HUE API gives always a respond for config request, but it only contains 8 Elements if no authorized user is used
+                if let resultValueJSON = data as? JSON {
+                    
+                    return resultValueJSON.count <= 8 // HUE API gives always a respond for config request, but it only contains 8 Elements if no authorized user is used
+                }
+                
+            default:
+                
+                return data as? [JSON] != nil // Errros are delivered as Array
             }
-            
-        default:
-            
-            return result.value as? [JSON] != nil // Errros are delivered as Array
         }
         
         return false
@@ -219,3 +220,102 @@ public class HeartbeatManager {
         }
     }
 }
+//
+////
+////  Result.swift
+////
+////  Copyright (c) 2014-2016 Alamofire Software Foundation (http://alamofire.org/)
+////
+////  Permission is hereby granted, free of charge, to any person obtaining a copy
+////  of this software and associated documentation files (the "Software"), to deal
+////  in the Software without restriction, including without limitation the rights
+////  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+////  copies of the Software, and to permit persons to whom the Software is
+////  furnished to do so, subject to the following conditions:
+////
+////  The above copyright notice and this permission notice shall be included in
+////  all copies or substantial portions of the Software.
+////
+////  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+////  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+////  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+////  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+////  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+////  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+////  THE SOFTWARE.
+////
+//
+///// Used to represent whether a request was successful or encountered an error.
+/////
+///// - success: The request and all post processing operations were successful resulting in the serialization of the
+/////            provided associated value.
+/////
+///// - failure: The request encountered an error resulting in a failure. The associated values are the original data
+/////            provided by the server as well as the error that caused the failure.
+//public enum Result<Value> {
+//    case success(Value)
+//    case failure(Error)
+//
+//    /// Returns `true` if the result is a success, `false` otherwise.
+//    public var isSuccess: Bool {
+//        switch self {
+//        case .success:
+//            return true
+//        case .failure:
+//            return false
+//        }
+//    }
+//
+//    /// Returns `true` if the result is a failure, `false` otherwise.
+//    public var isFailure: Bool {
+//        return !isSuccess
+//    }
+//
+//    /// Returns the associated value if the result is a success, `nil` otherwise.
+//    public var value: Value? {
+//        switch self {
+//        case .success(let value):
+//            return value
+//        case .failure:
+//            return nil
+//        }
+//    }
+//
+//    /// Returns the associated error value if the result is a failure, `nil` otherwise.
+//    public var error: Error? {
+//        switch self {
+//        case .success:
+//            return nil
+//        case .failure(let error):
+//            return error
+//        }
+//    }
+//}
+//
+//// MARK: - CustomStringConvertible
+//extension Result: CustomStringConvertible {
+//    /// The textual representation used when written to an output stream, which includes whether the result was a
+//    /// success or failure.
+//    public var description: String {
+//        switch self {
+//        case .success:
+//            return "SUCCESS"
+//        case .failure:
+//            return "FAILURE"
+//        }
+//    }
+//}
+//
+//// MARK: - CustomDebugStringConvertible
+//extension Result: CustomDebugStringConvertible {
+//    /// The debug textual representation used when written to an output stream, which includes whether the result was a
+//    /// success or failure in addition to the value or error.
+//    public var debugDescription: String {
+//        switch self {
+//        case .success(let value):
+//            return "SUCCESS: \(value)"
+//        case .failure(let error):
+//            return "FAILURE: \(error)"
+//        }
+//    }
+//}
